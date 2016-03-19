b7924f0ee6b95a1f7c636a8fe48e14defa47f063
Pablo Lagioia <pablo.lagioia@gmail.com>
2016-03-19 13:02:07 -0300

    updated styles

--- main.js
+++ main.js
@@ -4,26 +4,58 @@ $(document).ready(function() {
 
     event.preventDefault();
 
-    var firstName = $("#first-name").val();
-    var lastName = $("#last-name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
+    var firstName = $("#first-name");
+    var lastName = $("#last-name");
+    var email = $("#email");
+    var message = $("#message");
 
-    if (!firstName) {
-      $("#first-name").parents(".form-group").addClass("has-error");
+    var formGroupFirstName = firstName.parents(".form-group");
+    var formGroupLastName = lastName.parents(".form-group");
+    var formGroupEmail = email.parents(".form-group");
+    var formGroupMessage = message.parents(".form-group");
+
+    formGroupFirstName.removeClass("has-error").find(".tooltip-custom").addClass("hidden");
+    formGroupLastName.removeClass("has-error").find(".tooltip-custom").addClass("hidden");
+    formGroupEmail.removeClass("has-error").find(".tooltip-custom").addClass("hidden");
+    formGroupMessage.removeClass("has-error").find(".tooltip-custom").addClass("hidden");
+
+    if (!firstName.val()) {
+      formGroupFirstName.addClass("has-error");
+      formGroupFirstName.find(".tooltip-custom").removeClass("hidden");
       return;
+    } else {
+      formGroupFirstName.addClass("has-success");
     }
-    if (!lastName) {
-      alert("Debe escribir un apellido");
+    if (!lastName.val()) {
+      formGroupLastName.addClass("has-error");
+      formGroupLastName.find(".tooltip-custom").removeClass("hidden");
       return;
+    } else {
+      formGroupLastName.addClass("has-success");
     }
-    if (!email || !email.match(/[\w+\.]+@\w+\.\w+/ig)) {
-      alert("Debe escribir un email");
+
+    if (!email.val()) {
+      formGroupEmail.addClass("has-error");
+      formGroupEmail.find(".tooltip-custom[data-type=no-email]").removeClass("hidden");
       return;
+    } else {
+      formGroupEmail.addClass("has-success");
     }
-    if (!message) {
-      alert("Debe escribir un mensaje");
+    
+    if (!email.val().match(/[\w+\.]+@\w+\.\w+/ig)) {
+      formGroupEmail.addClass("has-error");
+      formGroupEmail.find(".tooltip-custom[data-type=invalid-email]").removeClass("hidden");
       return;
+    } else {
+      formGroupEmail.addClass("has-success");
+    }
+
+    if (!message.val()) {
+      formGroupMessage.addClass("has-error");
+      formGroupMessage.find(".tooltip-custom").removeClass("hidden");
+      return;
+    } else {
+      formGroupMessage.addClass("has-success");
     }
 
     //Aca va el codigo para mandar el mail
