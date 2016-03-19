6edd20b9dad4c599997206cbf4649f0b0df708d4
Pablo Lagioia <pablo.lagioia@gmail.com>
2016-03-19 11:19:42 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+  $("#contact-form [type=submit]").on("click", function(event) {
+
+    event.preventDefault();
+
+    var firstName = $("#first-name").val();
+    var lastName = $("#last-name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    if (!firstName) {
+      alert("Debe escribir un nombre");
+      return;
+    }
+    if (!lastName) {
+      alert("Debe escribir un apellido");
+      return;
+    }
+    if (!email || !email.match(/[\w+\.]+@\w+\.\w+/ig)) {
+      alert("Debe escribir un email");
+      return;
+    }
+    if (!message) {
+      alert("Debe escribir un mensaje");
+      return;
+    }
+
+    //Aca va el codigo para mandar el mail
+    throw new Error("Not implemented yet");
+
+  });
+
+});
